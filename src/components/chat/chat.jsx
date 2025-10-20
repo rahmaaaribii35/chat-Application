b0b62ca4137a269bc0c66dfe998be10aac6a3cc9
RahmaAribi <rahmaaribi35@gmail.com>
2025-10-20 12:58:15 +0100

    chat component design

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -4,7 +4,22 @@ import './chat.css'
 const Chat = () => {
   return (
     <div className='chat'>
-      Chat
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Hanine Baccar</span>
+            <p>Lorem ipsum dolor sit amet. </p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
     </div>
   )
 }
